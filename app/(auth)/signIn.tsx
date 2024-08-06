8268af9f9c27fcda96766fc81c810a605636eb77
Tiago Cunha <tiago92cunha@gmail.com>
2024-08-06 23:50:00 +0100

    Add intro app before login WIP

--- app/(auth)/signIn.tsx
+++ app/(auth)/signIn.tsx
@@ -6,7 +6,13 @@ import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
 import { useOAuth } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
 import React from "react";
-import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 enum Strategy {
   Google = "oauth_google",
@@ -43,54 +49,82 @@ const SignIn = () => {
     }
   };
 
+  const data = [
+    {
+      title: "Create your journey",
+      text: "Add destinations, the amount of days you want to travel, save your favorite spots, and start planning your trip!",
+      illustration: "",
+      isAuth: false,
+    },
+    {
+      title: "Visit your favorite spots",
+      text: "Create your journey map and visit your spots using your favorite navigation app.",
+      illustration: "",
+      isAuth: false,
+    },
+    {
+      title: "Create your journey",
+      text: "Add destinations, the amount of days you want to travel, save your favorite spots, and start planning your trip!",
+      illustration: "",
+      isAuth: true,
+    },
+  ];
+
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View
-        style={{ gap: 24 }}
-        className="flex-1 p-3 justify-center items-center bg-white"
-      >
-        <Text className="text-4xl text-sky-500 font-bold">Sign in</Text>
-        <Text className="text-xl text-sky-500">
-          What did you use to access your account?
-        </Text>
-        <TouchableOpacity
-          style={{ gap: 8 }}
-          onPress={() => onSelectAuth(Strategy.Apple)}
-          className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-black w-full"
-        >
-          <Apple height={24} width={24} />
-          <Text className="text-white text-xl">Sign in with Apple</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ gap: 8 }}
-          onPress={() => onSelectAuth(Strategy.Facebook)}
-          className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-[#1877F2] w-full"
-        >
-          <Facebook height={24} width={24} />
-          <Text className="text-white text-xl">Sign in with Facebook</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => onSelectAuth(Strategy.Google)}
-          style={{ gap: 8 }}
-          className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-white w-full border"
-        >
-          <Google height={24} width={24} />
-          <Text className="text-black text-xl">Sign in with Gmail</Text>
-        </TouchableOpacity>
-        {/* <View
-          style={{ gap: 12 }}
-          className="flex flex-row justify-center items-center"
-        >
-          <View className="h-[1px] bg-gray-300 flex-1"></View>
-          <Text className="text-base text-sky-500">Or</Text>
-          <View className="h-[1px] bg-gray-300 flex-1"></View>
-        </View>
-        <Button
-          onPress={() => router.push("/(home)")}
-          title="Sign up"
-          fullWidth
-        ></Button> */}
-      </View>
+      <FlatList
+        data={data}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        // className="w-full"
+        pagingEnabled
+        renderItem={({ item }) => {
+          if (item.isAuth)
+            return (
+              <View
+                style={{ gap: 24 }}
+                className="flex-1 w-screen p-3 justify-center items-center bg-white"
+              >
+                <Text className="text-4xl text-sky-500 font-bold">Sign in</Text>
+                <Text className="text-xl text-sky-500">
+                  What did you use to access your account?
+                </Text>
+                <TouchableOpacity
+                  style={{ gap: 8 }}
+                  onPress={() => onSelectAuth(Strategy.Apple)}
+                  className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-black w-full"
+                >
+                  <Apple height={24} width={24} />
+                  <Text className="text-white text-xl">Sign in with Apple</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{ gap: 8 }}
+                  onPress={() => onSelectAuth(Strategy.Facebook)}
+                  className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-[#1877F2] w-full"
+                >
+                  <Facebook height={24} width={24} />
+                  <Text className="text-white text-xl">
+                    Sign in with Facebook
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => onSelectAuth(Strategy.Google)}
+                  style={{ gap: 8 }}
+                  className="flex-row items-center justify-center h-12 px-4 py-2 rounded-2xl text-lg font-medium bg-white w-full border"
+                >
+                  <Google height={24} width={24} />
+                  <Text className="text-black text-xl">Sign in with Gmail</Text>
+                </TouchableOpacity>
+              </View>
+            );
+
+          return (
+            <View className="flex-1 w-screen bg-white justify-center items-center">
+              <Text>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 };
